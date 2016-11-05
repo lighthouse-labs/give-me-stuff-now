84c2f2a01c69584277587a01e6ecf0913f1a25db
James Sapara <james.sapara@gmail.com>
2016-11-04 17:30:42 -0700

    added a tranform to delay data streaming a little

--- server.js
+++ server.js
@@ -1,11 +1,20 @@
 const http = require('http');
 const fs = require('fs');
 const url = require('url');
+const Transform = require('stream').Transform
 const PORT = process.env.PORT || 3000;
 
+class T extends Transform {
+  _transform(data, enc, next) {
+    setTimeout(() => {
+      this.push(data);
+      next()
+    }, 1000);
+  }
+}
 
 function streamFile(res) {
-  fs.createReadStream('./text/life_of_brian.txt').pipe(res);
+  fs.createReadStream('./text/life_of_brian.txt').pipe(new T()).pipe(res);
 }
 
 const server = http.createServer((req, res) => {
